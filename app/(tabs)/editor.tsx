378004d39b2c7f2d620bf7237a8acf618dcdfacc
Daniel Machado <daniel.pmachado@hotmail.com>
2025-05-23 18:33:37 +0100

    tiptap editor

---
+++ app/(tabs)/editor.tsx
@@ -0,0 +1,6 @@
+"use dom";
+import { SimpleEditor } from "@/components/dom-components/tiptap-templates/simple/simple-editor";
+
+export default function Page() {
+  return <SimpleEditor />;
+}
---
+++ components/dom-components/tiptap.tsx
@@ -0,0 +1,6 @@
+"use dom";
+import { SimpleEditor } from "./tiptap-templates/simple/simple-editor";
+
+export default function Page() {
+  return <SimpleEditor />;
+}
